e1cf278d32d197055351ba5a018fd69bc1437801
Szczupi285 <bartekbiexd@gmail.com>
2023-11-24 19:27:47 +0100

    Added documentation for AddPostAsync, AddTagsAsync, AddPostTagsAsync

--- Project_Fourm/Services/PostService.cs
+++ Project_Fourm/Services/PostService.cs
@@ -24,6 +24,19 @@ namespace Project_Forum.Services
         }
 
 
+        /// <summary>
+        /// Asynchronously adds a new post to the database.
+        /// </summary>
+        /// <param name="userId">The identifier of the user creating the post.</param>
+        /// <param name="postContent">The content of the post.</param>
+        /// <returns>
+        /// An integer representing the unique identifier (PostId) assigned to the newly added post.
+        /// </returns>
+        /// <remarks>
+        /// Creates a new post entity with the provided user ID and post content, <br></br>
+        /// adds it to the database, and asynchronously saves the changes.
+        /// </remarks>
+        /// <returns>The unique identifier (PostId) assigned to the newly added post.</returns>
         public async Task<int> AddPostAsync(string userId, string postContent)
         {
            
@@ -39,8 +52,19 @@ namespace Project_Forum.Services
             return _Post.PostId;
         }
 
-       
 
+        /// <summary>
+        /// extracts and Asynchronously adds tags from the given post content to the database.
+        /// </summary>
+        /// <param name="postContent">The content of the post from which tags are to be extracted.</param>
+        /// <returns>
+        /// A HashSet of strings representing the extracted tags.
+        /// </returns>
+        /// <remarks>
+        /// This method uses a tag extractor utility to identify and extract tags from the provided post content.
+        /// It then checks if each tag already exists in the database and adds non-existing tags. <br></br>
+        /// </remarks>
+        /// <returns>A HashSet of strings representing the extracted tags.</returns>
         public async Task<HashSet<string>> AddTagsAsync(string postContent)
         {
             var Tags = TagExtractor.ExtractTags(postContent);
@@ -65,6 +89,23 @@ namespace Project_Forum.Services
 
         }
 
+        /// <summary>
+        /// Asynchronously adds associations between a post and its corresponding tags to the database.
+        /// </summary>
+        /// <param name="postId">The unique identifier of the post.</param>
+        /// <param name="tags">A HashSet of strings representing the tags to associate with the post.</param>
+        /// <returns>
+        /// A HashSet of tuples representing the unique identifiers (PostId, TagName) assigned to the added associations.
+        /// </returns>
+        /// <remarks>
+        /// Method takes a Post identifier and a HashSet of tags, checks if each tag exists in the database,
+        /// and creates associations between the post and existing tags.
+        /// It returns a HashSet of tuples representing the composite key of the added associations
+        /// and saves changes to the database.
+        /// </remarks>
+        /// <returns>
+        /// A HashSet of tuples representing the composite key (PostId, TagName) assigned to the added associations.
+        /// </returns>
         public async Task<HashSet<(int, string)>> AddPostTagsAsync(int postId, HashSet<string> tags)
         {
 
@@ -91,6 +132,8 @@ namespace Project_Forum.Services
             return PostTagPKs;
         }
             
+
+
 
         public Task AddUpvoteAsync()
         {
