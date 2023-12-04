b6891037a12943c1eaf1c51e5ecc982e0bdf82b4
Szczupi285 <bartekbiexd@gmail.com>
2023-12-04 16:06:47 +0100

    Reverted PostCompositeModel

---
+++ Project_Fourm/Models/PostCompositeModel.cs
@@ -0,0 +1,20 @@
+﻿using Project_Forum.Models.Entities;
+
+namespace Project_Forum.Models
+{
+    public class PostCompositeModel
+    {
+        public Post PostModel { get; set; } = new Post();
+
+        public FilterPosts FilterPostsModel { get; set; } = new FilterPosts();
+
+        public ApplicationUser UserModel { get; set; } = new ApplicationUser();
+
+
+        public PostUpvote PostUpvoteModel { get; set; } = new PostUpvote();
+
+        public Respond RespondModel { get; set; } = new Respond();
+
+        public RespondUpvote RespondUpvoteModel { get; set; } = new RespondUpvote();
+    }
+}
