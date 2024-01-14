e5ee330a6d15f376261574ae0ec4486f03769165
Szczupi285 <bartekbiexd@gmail.com>
2024-01-14 20:58:08 +0100

    Changed layout for guest views

---
+++ Project_Fourm/Controllers/FeedController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_Forum.Controllers
+{
+    public class FeedController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
