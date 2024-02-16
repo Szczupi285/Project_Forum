2c7c0d50e21958a3c69f08f48236178e88f5aa4d
Szczupi285 <bartekbiexd@gmail.com>
2024-02-17 00:23:11 +0100

    Sidebar layout now keeps its state(normal/collapsed)

--- Project_Fourm/wwwroot/js/SidebarHideScript.js
+++ Project_Fourm/wwwroot/js/SidebarHideScript.js
@@ -5,13 +5,26 @@ const sidebarCollapsed = "sidebar--collapsed";
 const sidebar = document.querySelector(".sidebar")
 const sidebarHide = sidebar.querySelector(".sidebar_hide");
 
+if (localStorage.getItem('SidebarState') === "chevron_right") {
+    sidebar.classList.add(sidebarCollapsed);
+    iconElement.textContent = "chevron_right";
+} else {
+    sidebar.classList.remove(sidebarCollapsed);
+    iconElement.textContent = "chevron_left";
+}
+
 
 
 sidebarHide.addEventListener("click", () => {
-sidebar.classList.toggle(sidebarCollapsed);
+    sidebar.classList.toggle(sidebarCollapsed);
 
-if (iconElement.textContent === "chevron_left")
-iconElement.textContent = "chevron_right";
-else
-iconElement.textContent = "chevron_left";
-        });
+    if (localStorage.getItem('SidebarState') === "chevron_left"){
+        iconElement.textContent = "chevron_right";
+        localStorage.setItem('SidebarState', iconElement.textContent);
+
+    }
+    else{
+        iconElement.textContent = "chevron_left";
+        localStorage.setItem('SidebarState', iconElement.textContent);
+    }
+});
